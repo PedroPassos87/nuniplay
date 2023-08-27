513bf98b34abae06eb6c098bdb1f61f74e460581
Mysterion147 <joaopedromaciel0@gmail.com>
2023-08-27 16:29:43 -0300

    main page e post levemente refatorados

--- lib/components/post_secenhanced.dart
+++ lib/components/post_secenhanced.dart
@@ -9,27 +9,82 @@ class EnhancedPost extends StatelessWidget {
   final String gameCategory;
 
   const EnhancedPost({
-    super.key,
+    Key? key,
     required this.title,
     required this.body,
     required this.user,
     required this.gameCategory,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(gameCategory),
-            Text(user),
-            Text(title),
-            Text(body),
-          ],
-        ),
-      ],
+    return Container(
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 206, 200, 200),
+        borderRadius: BorderRadius.circular(7),
+      ),
+      margin: EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+      padding: EdgeInsets.all(20),
+      child: Row(
+        crossAxisAlignment:
+            CrossAxisAlignment.start, // Alinhar o topo dos elementos
+        children: [
+          // profile pic / icon
+          Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.grey[400],
+            ),
+            padding: EdgeInsets.all(15),
+            child: Icon(
+              Icons.person,
+              color: Color.fromARGB(255, 242, 2, 174),
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          // post and who posted
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                gameCategory,
+                style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                user,
+                style: TextStyle(
+                  color: Colors.lightBlue,
+                  fontSize: 14,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                title,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                body,
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
