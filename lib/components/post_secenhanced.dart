91d7c05f8c5380ec85d02f2857c642821ed8df82
PedroPassos07 <peagacarreira@gmail.com>
2023-09-25 17:05:59 -0300

    random imagens

--- lib/components/post_secenhanced.dart
+++ lib/components/post_secenhanced.dart
@@ -1,8 +1,27 @@
-// CURRENTLY USED POST !!!!!
-
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
+List<String> profileImageURLs = [
+  'https://i.pinimg.com/564x/95/d0/fb/95d0fbaca7f906045b811ae729d682f8.jpg',
+  'https://i.pinimg.com/564x/64/ef/98/64ef980138c10eed414880785c501820.jpg',
+  'https://i.pinimg.com/564x/9a/1a/ac/9a1aac45106ec0f0ae5ba9f586fcca6b.jpg',
+  'https://i.pinimg.com/564x/80/f3/dc/80f3dc1bcb7d55f4a5ede676672f3df0.jpg',
+  'https://i.pinimg.com/736x/e7/71/f6/e771f6581c14e35ed4e17fc81bf9312e.jpg',
+];
+
+String selectedProfileImageURL =
+    'https://i.pinimg.com/564x/60/5e/46/605e461ca634e868085d1a3d9d02b1ea.jpg';
+
+String selectRandomProfileImage() {
+  final random = Random();
+  final randomIndex = random.nextInt(profileImageURLs.length);
+  selectedProfileImageURL = profileImageURLs[randomIndex];
+
+  return selectedProfileImageURL;
+}
 
 class EnhancedPost extends StatelessWidget {
   final String title;
@@ -20,6 +39,7 @@ class EnhancedPost extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _image = selectedProfileImageURL;
     return Container(
       decoration: BoxDecoration(
         color: Color.fromARGB(255, 245, 242, 242),
@@ -37,11 +57,11 @@ class EnhancedPost extends StatelessWidget {
               shape: BoxShape.circle,
               color: Color.fromARGB(255, 224, 223, 223),
             ),
-            padding: EdgeInsets.all(15),
-            child: Icon(
-              Icons.person,
-              color: Color.fromARGB(255, 242, 2, 174),
-              size: 40,
+            padding: EdgeInsets.all(12),
+            child: CircleAvatar(
+              backgroundColor: const Color.fromARGB(0, 194, 12, 12),
+              backgroundImage: NetworkImage(_image),
+              radius: 20, // Define um raio inicial (pode ser ajustado)
             ),
           ),
           SizedBox(
